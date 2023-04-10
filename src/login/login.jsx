8221869c99559b0d387baff4fdc09841db154b64
Ngwe Becky <ngwebecky96@gmail.com>
2023-04-10 14:17:18 +0100

    call the general function format

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,45 +1,51 @@
-import { Button, Checkbox, Container, Input, InputLabel } from "@material-ui/core";
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-
-function App() {
+export const Register = () => {
     return (
-        <Container fluid className="p-3 my-5 h-custom">
-            <Row>
-                {/* <Col col='10' md='6'>
-                    <img src="" className="img-fluid" alt="ASTEP Logo"/>
-                </Col> */}
-                <Col sm='6' className='d-none d-sm-block px-0'>
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
-                        alt="ASTEP Logo" className="w-100" style={{objectFit: 'cover', objectPosition: 'left'}} />
-                </Col>
-
-                <Col col='4' md='6'>
-                    <Card className='my-5'>
-                        <CardBody className='p-5'>
-                            <Row>
-                                            
-                                <Input wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-                                <Input wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
-
-                                <div className="text-center pt-1 mb-5 pb-1">
-                                    <Button className="mb-4 w-100 gradient-custom-2">Login</Button>
-                                </div>
-
-                                <div className="d-flex justify-content-between mb-4">
-                                    <Checkbox name="flexCheck" value='' id='flexCheckDefault' label='Remember Me'/>
-                                    <a href="!#">Forgot password?</a>
-                                </div>
-
-                                <div className="d-flex flex-column justify-content-center h-custom-2 w-75 pt-4">
-                                    <p className='ms-5'>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
-                                </div>
-                            </Row>
-                        </CardBody>
-                    </Card>
-                </Col>
-
-            </Row>
-        </Container>
+        <>Login</>
     )
 }
+
+// import { Button, Checkbox, Container, Input, InputLabel } from "@material-ui/core";
+// import React from "react";
+// import { Col, Row } from "react-bootstrap";
+
+// function App() {
+//     return (
+//         <Container fluid className="p-3 my-5 h-custom">
+//             <Row>
+//                 {/* <Col col='10' md='6'>
+//                     <img src="" className="img-fluid" alt="ASTEP Logo"/>
+//                 </Col> */}
+//                 <Col sm='6' className='d-none d-sm-block px-0'>
+//                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+//                         alt="ASTEP Logo" className="w-100" style={{objectFit: 'cover', objectPosition: 'left'}} />
+//                 </Col>
+
+//                 <Col col='4' md='6'>
+//                     <Card className='my-5'>
+//                         <CardBody className='p-5'>
+//                             <Row>
+                                            
+//                                 <Input wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+//                                 <Input wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+//                                 <div className="text-center pt-1 mb-5 pb-1">
+//                                     <Button className="mb-4 w-100 gradient-custom-2">Login</Button>
+//                                 </div>
+
+//                                 <div className="d-flex justify-content-between mb-4">
+//                                     <Checkbox name="flexCheck" value='' id='flexCheckDefault' label='Remember Me'/>
+//                                     <a href="!#">Forgot password?</a>
+//                                 </div>
+
+//                                 <div className="d-flex flex-column justify-content-center h-custom-2 w-75 pt-4">
+//                                     <p className='ms-5'>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+//                                 </div>
+//                             </Row>
+//                         </CardBody>
+//                     </Card>
+//                 </Col>
+
+//             </Row>
+//         </Container>
+//     )
+// }
