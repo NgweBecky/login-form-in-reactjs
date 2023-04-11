1bb16a0328e66c41e431b3babf7099f4147cbb0e
Ngwe Becky <ngwebecky96@gmail.com>
2023-04-11 10:43:39 +0100

    code clean up

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,22 @@
-export const Register = () => {
+import React, {useState} from "react"
+
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = () => {
+        // console
+    }
     return (
-        <>Login</>
+        <form>
+            <label for="email">email</label>
+            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        
+            <label for="password">password</label>
+            <input value={pass} type="password" placeholder="********" id="password" name="password" />
+        
+            <button>Log In</button>
+        </form>
     )
 }
 
