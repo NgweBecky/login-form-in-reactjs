e561f61fd0669de95875a124a8a58399544fa5f3
Ngwe Becky <ngwebecky96@gmail.com>
2023-04-11 11:04:54 +0100

    code clean up

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,18 +4,19 @@ export const Login = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const handleSubmit = () => {
-        // console
+    const handleSubmit = (e) => {
+        //e.preventDefault();
+        console.log(email);
     }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label for="email">email</label>
-            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         
             <label for="password">password</label>
-            <input value={pass} type="password" placeholder="********" id="password" name="password" />
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
         
-            <button>Log In</button>
+            <button type="submit">Log In</button>
         </form>
     )
 }
